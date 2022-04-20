8005d417ca162cb5ee56e647b2286931a70f5aac
Piotr Orczykowski <228820@edu.p.lodz.pl>
2022-04-20 21:43:57 +0200

    Add rooms components and rooms view

---
+++ src/components/RoomCard.vue
@@ -0,0 +1,82 @@
+<template>
+    <v-card
+    >
+    <!-- Here wil be room photo -->
+        <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        ></v-img>
+
+        <v-card-title>
+            {{ this.rooms.title }}
+        </v-card-title>
+
+        <v-card-subtitle>
+            {{ this.rooms.description }}
+        </v-card-subtitle>
+
+        <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0"
+            >
+                <v-rating
+                    v-model=this.rooms.rating
+                    color="amber"
+                    half-increments
+                    length="5"
+                    readonly
+                    size="20"
+                ></v-rating>
+                <div class="grey--text ms-4">
+                    {{ this.rooms.rating }} ({{ this.rooms.countOfRatings }})
+                </div>
+            </v-row>
+        </v-card-text>
+
+        <v-card-text>
+            <v-row
+                align="center"
+                class="mx-0"
+            >
+                <p class="font-weight-black">
+                    $ {{ this.rooms.price }}
+                </p>
+                /night
+            </v-row>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-btn text>
+                More
+            </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+                icon
+                @click="show = !show"
+            >
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+        </v-card-actions>
+
+        <v-expand-transition>
+            <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text>
+                    {{ this.rooms.extendedDescription }}
+                </v-card-text>
+            </div>
+        </v-expand-transition>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: ['rooms'],
+    data: () => ({
+        show: false,
+    }),
+}
+</script>
---
+++ src/views/RoomsView.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-container fluid>
+        <v-row v-bind:key="n" v-for="n in rooms.length">
+            <v-col 
+                v-bind:key="rooms[n-1][m-1]"
+                v-for="m in rooms[n-1].length"
+                cols="12"
+                sm="4"
+            >
+                <RoomCard 
+                    v-bind:rooms="rooms[n-1][m-1]"
+                />
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import RoomCard from '../components/RoomCard.vue';
+
+export default {
+    name: 'RoomView',
+    components: {
+        RoomCard,
+    },
+    data: () => ({
+        rooms: [
+            [{
+                title: 'test title 1',
+                description: 'test description 1',
+                rating: 3,
+                countOfRatings: 10,
+                price: 120,
+                extendedDescription: 'test extended description 1'
+            },
+            {
+                title: 'test title 2',
+                description: 'test description 2',
+                rating: 4,
+                countOfRatings: 100,
+                price: 40,
+                extendedDescription: 'test extended description 2'
+            },
+            {
+                title: 'test title 3',
+                description: 'test description 3',
+                rating: 2,
+                countOfRatings: 1,
+                price: 200,
+                extendedDescription: 'test extended description 3'
+            }],
+            [{
+                title: 'test title 4',
+                description: 'test description 4',
+                rating: 5,
+                countOfRatings: 30,
+                price: 80,
+                extendedDescription: 'test extended description 4'
+            },                        
+            {
+                title: 'test title 5',
+                description: 'test description 5',
+                rating: 1,
+                countOfRatings: 20,
+                price: 50,
+                extendedDescription: 'test extended description 5'
+            }],
+            
+        ], // Here will be rooms info from backend
+    }),
+};
+</script>
