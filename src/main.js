7b8590ea88f0b0f686f713d434b83abf9c6ee8c1
Grzegorz Kędzierski <kedzierskigrzegorz1999@gmail.com>
2022-05-04 06:54:03 +0200

    Added JWT authentication

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import { loadFonts } from './plugins/webfontloader'
+import setupInterceptors from "./services/setupInterceptors";
 
+setupInterceptors();
 loadFonts()
 
 createApp(App)
--- src/services/admin.service.js
+++ src/services/admin.service.js
@@ -1,18 +1,33 @@
-import axios from "axios";
 import environment from "../environment";
+import api from './api'
 
 const AdminService = {
-    baseHref: environment.apiUrl + 'administration/',
+    baseHref: 'administration/',
     userLocalStorageKey: 'admin-session-data',
+
+    getAdminSessionData() {
+        return JSON.parse(localStorage.getItem(this.userLocalStorageKey));
+    },
 
     async signIn(loginOrEmail, password) {
         try {
-            const result = await axios.post(
+            const result = await api.post(
                 this.baseHref + 'login',
                 {loginOrEmail, password}
             );
 
+            localStorage.setItem(this.userLocalStorageKey, JSON.stringify(result.data));
+
             return result.data;
+        } catch (e) {
+            console.error(e);
+        }
+    },
+
+    async test() {
+        try {
+            const result = await api.get(this.baseHref + 'test');
+            console.log(result);
         } catch (e) {
             console.error(e);
         }
