3c4ccad0a88b88aa8479206abcac81df98ef8df7
Piotr Orczykowski <228820@edu.p.lodz.pl>
2022-05-09 20:22:53 +0200

    Add component for facilities

---
+++ src/components/FacilitiesIcons.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+    <v-btn
+        class="facilities"
+        elevation="2"
+        icon
+        color="brown"
+    >
+        <v-icon dark>
+            mdi-wifi
+        </v-icon>
+    </v-btn>
+
+    <v-btn
+        class="facilities"
+        elevation="2"
+        icon
+        color="brown"
+    >
+        <v-icon dark>
+            mdi-parking
+        </v-icon>
+    </v-btn>
+    
+    <v-btn
+        class="facilities"
+        elevation="2"
+        icon
+        color="brown"
+    >
+        <v-icon>
+            mdi-dog-side
+        </v-icon>
+    </v-btn>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'FacilitiesIcons'
+}
+</script>
+
+<style scoped>
+.facilities {
+    margin-right: 2em;
+}
+</style>
