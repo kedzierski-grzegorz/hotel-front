f7c00e788151c20f7ca6054cc4681a58cdcd0beb
Piotr Orczykowski <228820@edu.p.lodz.pl>
2022-05-14 18:52:52 +0200

    Add some styles

--- src/components/MainAppSidebar.vue
+++ src/components/MainAppSidebar.vue
@@ -27,17 +27,21 @@
   </v-navigation-drawer>
   <!-- This coude above seems to be unused? -->
 
-  <v-app-bar app>
+  <v-app-bar app color="brown">
       <span class="hidden-sm-and-up">
         <v-app-bar-nav-icon @click="sidebar = !sidebar"></v-app-bar-nav-icon>
       </span>
     <v-toolbar-title>
-      <router-link to="/" tag="span" style="cursor: pointer">Hotello</router-link>
+      <router-link id="title" to="/" tag="span" 
+        style="text-decoration: none; color: inherit; cursor: pointer;">
+          Hotello
+      </router-link>
     </v-toolbar-title>
     <v-spacer></v-spacer>
     <div class="d-none d-sm-flex">
       <v-toolbar-items>
         <v-btn
+            class="button"
             v-for="item in menuItems"
             :key="item.title"
             :to="item.path">
@@ -64,5 +68,23 @@ export default {
 </script>
 
 <style scoped>
+#title {
+  font-weight: bold;
+  font-size: 2em;
+  letter-spacing: 0.15em;
+}
 
+#title:hover {
+  opacity: 0.9;
+}
+
+#title:active {
+  opacity: 1;
+}
+
+.button {
+  font-weight: bold;
+  font-size: 1em;
+  margin-left: 1em;
+}
 </style>
